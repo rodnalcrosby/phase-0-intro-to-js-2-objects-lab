103a920c41457162c8189631644d09bdbbafc258
Rodnal Crosby Jr <rodnalcrosby@gmail.com>
2023-10-23 17:24:03 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+const employee = {
+    name: 'Sam',
+    streetAddress: '123 Foraker, Toledo, Ohio',
+  };
+  
+  function updateEmployeeWithKeyAndValue(employee, key, value) {
+  return {
+    ...employee,
+    [key]: value
+  };
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+  }
+  
+  function deleteFromEmployeeByKey(employee, key) {
+    const updatedEmployee = { ...employee };
+    delete updatedEmployee[key];
+    return updatedEmployee;
+  }
+    function destructivelyDeleteFromEmployeeByKey(employee, key) {
+        delete employee[key];
+        return employee;
+      }
+      
